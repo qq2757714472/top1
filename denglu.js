57d7050954d1ba94b8ecbf4f6fee07dec9d434a9
qq2757714472 <2757714472@qq.com>
2024-05-26 11:58:52 +0800

    Add files via upload

---
+++ denglu.js
@@ -0,0 +1,12 @@
+ function functionClick(){
+	var wrapperValue=document.getElementById("wrapper");
+	wrapperValue.style.display="block";
+}
+//点击关闭按钮关闭登录窗口
+ function functionClick(){
+	 var wrapperValue=document.getElementById("wrapper");
+	 wrapperValue.style.display="none";
+	 btn.addEventListener('click',function()){
+						  alert("已登录");
+						  }
+ };
